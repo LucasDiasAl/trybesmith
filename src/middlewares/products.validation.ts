bdf2c333606688594fee91e081588fd1e01a3591
Lucas Dias <lucaalencarID@gmail.com>
2023-02-27 18:44:16 -0300

    requisito 06 add o middleware para validaçao de req ao pedir o cadastro de um novo produto

---
+++ src/middlewares/products.validation.ts
@@ -0,0 +1,64 @@
+import { Response, Request, NextFunction } from 'express';
+
+const properties = ['name', 'amount'];
+
+type Product = {
+  name: string,
+  amount: string,
+};
+
+export default class ProductsMiddleware {
+  static validateProperties = (product: Product): string | null => {
+    const entries = Object.entries(product);
+    for (let i = 0; i < properties.length; i += 1) {
+      if (!Object.prototype.hasOwnProperty.call(product, properties[i])) {
+        return properties[i];
+      }
+      const [property, value] = entries[i];
+      if (!value) return property;
+    }
+    return null;
+  };
+
+  static validateType = (product: Product): string | null => {
+    const entries = Object.entries(product);
+    for (let i = 0; i < entries.length; i += 1) {
+      const [property, value] = entries[i];
+      if (typeof value !== 'string') {
+        return property;
+      }
+    }
+    return null;
+  };
+  
+  static validateValues = (product: Product): string | null => {
+    const entries = Object.entries(product);
+    for (let i = 0; i < entries.length; i += 1) {
+      const [property, value] = entries[i];
+      if (value.length < 3) {
+        return property;
+      }
+    }
+    return null;
+  };
+
+  public productValidation = (req: Request, res: Response, next: NextFunction) => {
+    const product = req.body;
+    const propertiesIsValid = ProductsMiddleware.validateProperties(product);
+    if (propertiesIsValid) {
+      return res.status(400)
+        .json({ message: `"${propertiesIsValid}" is required` });
+    }
+    const typeIsValid = ProductsMiddleware.validateType(product);
+    if (typeIsValid) {
+      return res.status(422)
+        .json({ message: `"${typeIsValid}" must be a string` });
+    }
+    const valuesIsValid = ProductsMiddleware.validateValues(product);
+    if (valuesIsValid) {
+      return res.status(422)
+        .json({ message: `"${valuesIsValid}" length must be at least 3 characters long` });
+    }
+    next();
+  };
+}
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import ProductsMiddleware from '../middlewares/products.validation';
 
 const productsController = new ProductsController();
+const productMiddleware = new ProductsMiddleware();
 const router = Router();
 
-router.post('/', productsController.createProduct);
+router.post('/', productMiddleware.productValidation, productsController.createProduct);
 
 router.get('/', productsController.getAllProducts);
 
